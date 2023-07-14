48176513f120dda923cbc71268b627d85cc5787f
Yurim Yang <yang980329@likelion.org>
2023-07-14 17:14:29 +0900

    230714::part2-v20~v24 recommit

---
+++ src/part2/ch01/v20/ArrayTest.java
@@ -0,0 +1,19 @@
+package part2.ch01.v20;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int total = 0;
+
+
+        for(int i=0, num=1; i< arr.length; i++, num++) {
+            arr[i] = num;
+        }
+
+        for( int i =0; i<arr.length; i++) {
+            total += arr[i];
+        }
+        System.out.println(total);
+
+    }
+}
