f3b0fbdcf51e3921c88ebb457fc841675dc0a49c
Yurim Yang <yang980329@likelion.org>
2023-07-15 11:44:28 +0900

    230715::part6-ch02-v05

--- src/part6/ch02/v02/MyLinkedList.java
+++ src/part6/ch02/v02/MyLinkedList.java
@@ -47,37 +47,86 @@ public class MyLinkedList<T> implements Ilist<T> {
 
     @Override
     public boolean delete(T t) {
+        Node prev = this.head;
+        Node curr = prev.next;
+        while(curr!=null){
+            if (curr.data.equals(t)){
+                prev.next = curr.next;
+                curr.next = null;
+                this.size--;
+                return true;
+            }
+            prev = prev.next;
+            curr = curr.next;
+        }
         return false;
     }
 
     @Override
     public boolean deleteByIndex(int index) {
+        if (index >= this.size || index < 0){
+            throw new IndexOutOfBoundsException();
+        }
+        Node prev = this.head;
+        Node curr = prev.next;
+
+        int i = 0;
+        while (i++ < index) {
+            prev = prev.next;
+            curr = curr.next;
+        }
+
+        prev.next = curr.next;
+        curr.next = null;
+        this.size--;
         return false;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Node curr = this.head.next;
+        int i = 0;
+        while(i++ < index){
+            curr = curr.next;
+        }
+        return curr.data;
+
     }
 
     @Override
     public int indexOf(T t) {
-        return 0;
+        Node curr = this.head.next;
+        int index = 0;
+        while(curr!= null){
+            if(t.equals(curr.data)){
+                return index;
+            }
+            curr = curr.next;
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public boolean isEmptry() {
-        return false;
+        return this.head.next == null;
     }
 
     @Override
     public boolean contains(T t) {
+        Node curr =this.head.next;
+        while(curr != null){
+            if (t.equals(curr.data)){
+                return true;
+            }
+            curr = curr.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     private class Node{
