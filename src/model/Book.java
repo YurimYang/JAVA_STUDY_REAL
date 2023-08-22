5e659e703d9df15a1e719589a1db816e9aece218
Yurim Yang <yang980329@likelion.org>
2023-08-22 15:49:56 +0900

    230818::javaBasic 실시간 수업

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,2 +1,52 @@
-package model;public class Book {
+package model;
+
+public class Book {
+    private String title;
+    private String company;
+    private String name;
+    private int price;
+
+    public Book(){
+
+    }
+
+    public Book(String title, String name, String company, int price){
+        this.title = title;
+        this.name = name;
+        this.company = company;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
 }
